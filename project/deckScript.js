b4c52b962e3fa752b8a9d6d3971cc56f3c9d5d6d
Noah Klug <n.klug@htl-leonding.ac.at>
2025-04-29 13:36:59 +0200

    deck styling changes

--- project/deckScript.js
+++ project/deckScript.js
@@ -7,18 +7,18 @@ let deckSelectedBox = null;
 let deckAddedBoxes = new Set();
 
 const unitTypesArray = [
-  { type: "swordsman", cost: 3, speed: 20, hp: 600, damage: 150, width: 30, height: 30, color: "cyan", attackCooldown: 1.0, perceptionRadius: 150, attackRange: 20, imageRun: "img/sprites/barbarBlue.png", image: "img/sprites/barbarBlue.png", totalFrames: 2, frameSpeed: 25 },
-  { type: "archer", cost: 3, speed: 20, hp: 250, damage: 120, width: 30, height: 30, color: "green", attackCooldown: 1.2, perceptionRadius: 150, attackRange: 80, imageRun: "img/sprites/archerBlue.png", totalFrames: 2, frameSpeed: 25 },
-  { type: "giant", cost: 5, speed: 10, hp: 4232, damage: 100, width: 40, height: 40, color: "purple", attackCooldown: 2.0, perceptionRadius: 150, attackRange: 20, imageRun: "img/sprites/rieseBlue.png", totalFrames: 2, frameSpeed: 25 },
-  { type: "knight", cost: 4, speed: 20, hp: 1703, damage: 140, width: 30, height: 30, color: "yellow", attackCooldown: 1.0, perceptionRadius: 150, attackRange: 20, imageRun: "img/sprites/ritterBlue.png", totalFrames: 2, frameSpeed: 25 },
-  { type: "skeleton", cost: 1, speed: 30, hp: 100, damage: 100, width: 20, height: 20, color: "gray", attackCooldown: 0.8, perceptionRadius: 100, attackRange: 30, imageRun: "img/sprites/skelletBlue.png", totalFrames: 2, frameSpeed: 25 },
-  { type: "wizard", cost: 6, speed: 40, hp: 500, damage: 200, width: 30, height: 30, color: "magenta", attackCooldown: 1.5, perceptionRadius: 150, attackRange: 50, imageRun: "img/sprites/magierBlue.png", totalFrames: 2, frameSpeed: 25 },
-  { type: "minion", cost: 3, speed: 40, hp: 120, damage: 100, width: 25, height: 25, color: "pink", attackCooldown: 1.0, perceptionRadius: 150, attackRange: 40, imageRun: "img/sprites/barbarBlue.png", totalFrames: 2, frameSpeed: 25 },
-  { type: "goblin", cost: 2, speed: 40, hp: 200, damage: 100, width: 25, height: 25, color: "darkgreen", attackCooldown: 0.8, perceptionRadius: 150, attackRange: 30, imageRun: "img/sprites/barbarBlue.png", totalFrames: 2, frameSpeed: 25 },
-  { type: "assassin", cost: 4, speed: 50, hp: 200, damage: 300, width: 25, height: 25, color: "black", attackCooldown: 0.6, perceptionRadius: 200, attackRange: 30, imageRun: "img/sprites/assassin.png", totalFrames: 2, frameSpeed: 25 },
-  { type: "healer", cost: 3, speed: 25, hp: 300, damage: 0, width: 30, height: 30, color: "lightgreen", attackCooldown: 2.0, perceptionRadius: 150, attackRange: 60, imageRun: "img/sprites/healer.png", totalFrames: 2, frameSpeed: 25 },
-  { type: "tank", cost: 6, speed: 10, hp: 6000, damage: 50, width: 50, height: 50, color: "darkblue", attackCooldown: 2.5, perceptionRadius: 150, attackRange: 20, imageRun: "img/sprites/tank.png", totalFrames: 2, frameSpeed: 25 },
-  { type: "dragon", cost: 10, speed: 35, hp: 2000, damage: 500, width: 60, height: 60, color: "red", attackCooldown: 3.0, perceptionRadius: 200, attackRange: 100, imageRun: "img/sprites/dragon.png", totalFrames: 2, frameSpeed: 25 }
+  { type: "swordsman", cost: 3, speed: 20, hp: 600, damage: 150, width: 30, height: 30, color: "cyan", attackCooldown: 1.0, perceptionRadius: 150, attackRange: 20, image: "img/sprites/barbarBlue.png", totalFrames: 2, frameSpeed: 25 },
+  { type: "archer", cost: 3, speed: 20, hp: 250, damage: 120, width: 30, height: 30, color: "green", attackCooldown: 1.2, perceptionRadius: 150, attackRange: 80, image: "img/sprites/archerBlue.png", totalFrames: 2, frameSpeed: 25 },
+  { type: "giant", cost: 5, speed: 10, hp: 4232, damage: 100, width: 40, height: 40, color: "purple", attackCooldown: 2.0, perceptionRadius: 150, attackRange: 20, image: "img/sprites/rieseBlue.png", totalFrames: 2, frameSpeed: 25 },
+  { type: "knight", cost: 4, speed: 20, hp: 1703, damage: 140, width: 30, height: 30, color: "yellow", attackCooldown: 1.0, perceptionRadius: 150, attackRange: 20, image: "img/sprites/ritterBlue.png", totalFrames: 2, frameSpeed: 25 },
+  { type: "skeleton", cost: 1, speed: 30, hp: 100, damage: 100, width: 20, height: 20, color: "gray", attackCooldown: 0.8, perceptionRadius: 100, attackRange: 30, image: "img/sprites/skelletBlue.png", totalFrames: 2, frameSpeed: 25 },
+  { type: "wizard", cost: 6, speed: 40, hp: 500, damage: 200, width: 30, height: 30, color: "magenta", attackCooldown: 1.5, perceptionRadius: 150, attackRange: 50, image: "img/sprites/magierBlue.png", totalFrames: 2, frameSpeed: 25 },
+  { type: "wizardFire", cost: 3, speed: 40, hp: 120, damage: 100, width: 25, height: 25, color: "pink", attackCooldown: 1.0, perceptionRadius: 150, attackRange: 40, image: "img/sprites/magierFeuerBlue.png", totalFrames: 2, frameSpeed: 25 },
+  { type: "tableTennis", cost: 2, speed: 40, hp: 200, damage: 100, width: 25, height: 25, color: "darkgreen", attackCooldown: 0.8, perceptionRadius: 150, attackRange: 30, image: "img/sprites/tableTennisManBlue.png", totalFrames: 2, frameSpeed: 25 },
+  { type: "gunMan", cost: 4, speed: 50, hp: 200, damage: 300, width: 25, height: 25, color: "black", attackCooldown: 0.6, perceptionRadius: 200, attackRange: 30, image: "img/sprites/gunManBlue.png", totalFrames: 2, frameSpeed: 25 },
+  { type: "viking", cost: 3, speed: 25, hp: 300, damage: 0, width: 30, height: 30, color: "lightgreen", attackCooldown: 2.0, perceptionRadius: 150, attackRange: 60, image: "img/sprites/vikingBlue.png", totalFrames: 2, frameSpeed: 25 },
+  { type: "megaKnight", cost: 6, speed: 10, hp: 6000, damage: 50, width: 50, height: 50, color: "darkblue", attackCooldown: 2.5, perceptionRadius: 150, attackRange: 20, image: "img/sprites/megaRitterBlue.png", totalFrames: 2, frameSpeed: 25 },
+  { type: "witch", cost: 10, speed: 35, hp: 2000, damage: 500, width: 60, height: 60, color: "red", attackCooldown: 3.0, perceptionRadius: 200, attackRange: 100, image: "img/sprites/hexeBlue.png", totalFrames: 2, frameSpeed: 25 }
 ];
 
 
@@ -26,7 +26,7 @@ const unitTypesArray = [
 
 function initDeckBoxes() {
   deckBoxContainer.innerHTML = "";
-  for (let i = 1; i <= 12; i++) {
+  for (let i = 0; i <= 12; i++) {
     const box = createDeckBox(i);
     deckBoxContainer.appendChild(box);
   }
@@ -34,7 +34,7 @@ function initDeckBoxes() {
 function createDeckBox(number) {
   const container = document.createElement("div");
   container.className = "deckCardContainer";
-  container.setAttribute("data-box", `Box ${number}`);
+  container.setAttribute("data-box", `Box${number}`);
 
   // Inhalt der Karte als HTML (statt textContent)
   const card = document.createElement("div");
@@ -42,9 +42,9 @@ function createDeckBox(number) {
 
   card.innerHTML = `
   <div class="deckCardContent">
-    <img src="img/sprites/archerBlue.png" alt="Box ${number}" class="deckCardImage">
+    <img src="${unitTypesArray[number].image}" alt="Box${number}" class="deckCardImage">
     <div class="deckCardOverlay">
-      <h3>Box ${number}</h3>
+      <h3>${unitTypesArray[number].type.toUpperCase()}</h3>
     </div>
   </div>
 `;
@@ -59,7 +59,7 @@ function createDeckBox(number) {
   addBtn.textContent = "Hinzufügen";
   addBtn.onclick = (e) => {
     e.stopPropagation();
-    handleDeckAddBox(`Box ${number}`, container);
+    handleDeckAddBox(`Box${number}`, container);
   };
 
   // Info-Button
@@ -68,7 +68,7 @@ function createDeckBox(number) {
   infoBtn.textContent = "Info";
   infoBtn.onclick = (e) => {
     e.stopPropagation();
-    showDeckInfo(`Box ${number}`, `Infos zu Box ${number}`);
+    showDeckInfo(`Box${number}`, `${unitTypesArray[number].type.toUpperCase()}`);
   };
 
   // Buttons zum Container hinzufügen
@@ -88,14 +88,17 @@ function handleDeckAddBox(boxLabel, boxElement) {
 
   deckSelectedBox = boxLabel;
 
+  const index = parseInt(boxLabel.replace("Box", ""));
   const total = deckFrame1.children.length + deckFrame2.children.length;
+
   if (total < 8) {
+    // Direkt hinzufügen
     const newCard = document.createElement("div");
     newCard.className = "deckTargetCard";
     newCard.innerHTML = `
       <div class="deckTargetCardContent">
-        <img src="${imgUrls[0/*numberFromLabel(boxLabel)*/]}" alt="${boxLabel}" class="deckTargetImage">
-        <div class="deckTargetOverlay">${boxLabel}</div>
+        <img src="${unitTypesArray[index].image}" alt="${boxLabel}" class="deckTargetImage">
+        <div class="deckTargetOverlay"><h4>${unitTypesArray[index].type}</h4></div>
       </div>
     `;
     newCard.onclick = () => handleDeckReplaceBox(newCard);
@@ -117,7 +120,10 @@ function handleDeckAddBox(boxLabel, boxElement) {
 
     deckAddedBoxes.add(boxLabel);
     boxElement.remove();
+    deckSelectedBox = null;
   } else {
+    // Deck ist voll – Auswahl bleibt erhalten für Austausch
+    // Nur Animation – Karte nicht entfernen!
     document.getElementById("deckTargetFrame").scrollIntoView({ behavior: "smooth" });
     document.querySelectorAll(".deckTargetCard").forEach(card => {
       card.classList.add("deckWobble");
@@ -130,15 +136,16 @@ function handleDeckAddBox(boxLabel, boxElement) {
   }
 }
 
+
 function handleDeckReplaceBox(existingCard) {
   if (deckSelectedBox && !deckAddedBoxes.has(deckSelectedBox)) {
     const oldLabel = existingCard.querySelector(".deckTargetOverlay").textContent.trim();
-
+    const index = parseInt(deckSelectedBox.replace("Box", ""));
     // Erstelle neuen HTML-Inhalt mit Bild
     existingCard.innerHTML = `
       <div class="deckTargetCardContent">
-        <img src="${imgUrls[/*numberFromLabel(deckSelectedBox)*/0]}" alt="${deckSelectedBox}" class="deckTargetImage">
-        <div class="deckTargetOverlay">${deckSelectedBox}</div>
+        <img src="${unitTypesArray[index].image}" alt="${boxLabel}" class="deckTargetImage">
+        <div class="deckTargetOverlay">${unitTypesArray[index].type}</div>
       </div>
     `;
 
@@ -172,10 +179,24 @@ function handleDeckReplaceBox(existingCard) {
 function handleDeckRemoveBox(card, label) {
   card.remove();
   deckAddedBoxes.delete(label);
-  const number = parseInt(label.replace("Box ", ""));
-  const restored = createDeckBox(number);
-  deckBoxContainer.appendChild(restored);
+
+  const index = parseInt(label.replace("Box", ""));
+  const restored = createDeckBox(index);
+
+  // Die Karte an der ursprünglichen Position wieder einfügen
+  const allDecks = [...deckBoxContainer.children];
+  const insertBefore = allDecks.find(el => {
+    const elIndex = parseInt(el.getAttribute("data-box").replace("Box", ""));
+    return elIndex > index;
+  });
+
+  if (insertBefore) {
+    deckBoxContainer.insertBefore(restored, insertBefore);
+  } else {
+    deckBoxContainer.appendChild(restored);
+  }
 }
+
 
 function showDeckInfo(title, content) {
   document.getElementById("deckInfoTitle").textContent = title;
